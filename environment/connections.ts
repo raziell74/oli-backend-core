797170ddcef10cedde5747ef78b7762863e0f386
Jordan Richmeier <jordan.richmeier2@gmail.com>
2023-03-30 13:14:35 -0600

    Removing broken replica sets

--- environment/connections.ts
+++ environment/connections.ts
@@ -2,17 +2,11 @@ import env from './constants';
 import mongoose from 'mongoose';
 import * as redis from 'redis';
 
-export const conn_core = mongoose.createConnection(`${env.coreDbUrl}${env.coreDb}`, {
-  replicaSet: 'rs0',
-});
+export const conn_core = mongoose.createConnection(`${env.coreDbUrl}${env.coreDb}`);
 
-export const conn_reports = mongoose.createConnection(`${env.coreDbUrl}${env.reportsDb}`, {
-  replicaSet: 'rs0',
-});
+export const conn_reports = mongoose.createConnection(`${env.coreDbUrl}${env.reportsDb}`);
 
-export const conn_inspectors = mongoose.createConnection(`${env.coreDbUrl}${env.inspectorsDb}`, {
-  replicaSet: 'rs0',
-});
+export const conn_inspectors = mongoose.createConnection(`${env.coreDbUrl}${env.inspectorsDb}`);
 
 export const cache = redis.createClient({
   socket: {
