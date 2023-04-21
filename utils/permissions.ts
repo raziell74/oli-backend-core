c9e3264b8488578a296d3e17604e073b8470ad30
Jordan Richmeier <jordan.richmeier2@gmail.com>
2023-04-21 11:06:44 -0600

    Added new util files
    - Validation utility holds easy access to validation methods
    - Added isValidEmail function to validation util
    - Permissions utility that will check a user for specific user roles and reject them if they do not meet the criteria

---
+++ utils/permissions.ts
@@ -0,0 +1,50 @@
+// utils/permissions.ts
+import { ExpressContext } from 'apollo-server-express';
+import { ApolloError } from 'apollo-server-errors';
+import User from '../models/User';
+import ROLES from '../environment/userRoles';
+
+export interface Context extends ExpressContext {
+  ip: string;
+  authUserId: string;
+}
+
+/**
+ * checkPermission()
+ * Checks if a user has a specified role.
+ * Throws an ApolloError if it does not
+ *
+ * @param userId string
+ * @param roles string[]
+ * @returns void
+ */
+export const checkPermission = async (userId: string, roles: string[]): Promise<void> => {
+  const user = await User.findById(userId);
+  if (!user) {
+    throw new ApolloError('Authenticated User does not exist.', 'AUTH_USER_INVALID', {
+      _id: userId,
+    });
+  }
+
+  // early exit if user has the ADMIN role. ADMIN's have access to everything
+  if (user.roles.includes(ROLES.ADMIN)) return;
+
+  let hasRole = false;
+  roles.map((role) => {
+    if (user.roles.includes(role)) {
+      hasRole = true;
+    }
+  });
+
+  if (!hasRole) {
+    throw new ApolloError(
+      'You are not authorized to perform this action.',
+      'AUTH_USER_PERMISSION_DENIED',
+      {
+        checkedForRoles: roles,
+        username: user.username,
+        userRoles: user.roles,
+      }
+    );
+  }
+};
