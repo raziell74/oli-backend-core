83fa05e3a5060df965e391f3e5770a50a7aafa10
Jordan Richmeier <jordan.richmeier2@gmail.com>
2023-04-21 11:03:07 -0600

    Removing sample code

--- resolvers/mutations/sample.ts
+++
@@ -1,8 +0,0 @@
-import { SampleInput } from '../queries/sample';
-
-export const mutationSample = async (_parent: object, args: { input: SampleInput }) => {
-  const {
-    input: { isSuccess: success },
-  } = args;
-  return { success };
-};
